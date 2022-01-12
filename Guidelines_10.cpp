1daa6d3d1f12226ae0b55749327a32b50c936307
Avishkaar007 <avishkaarphoto@gmail.com>
2022-01-13 00:49:48 +0530

    Update Guidelines_10.cpp

--- Guidelines_10.cpp
+++ Guidelines_10.cpp
@@ -5,7 +5,16 @@ void even(int array[],int length);
 void odd(int array[],int length);
 void sum(int array[],int length);
 void average(int array[],int length);
+void choice(int array[],int length);
+void input();
+
 int main(){
+    
+    input();
+}//main
+
+void input()
+{
     int length;
     cout<<"Enter length of Array ";
     cin>>length;
@@ -15,8 +24,9 @@ int main(){
     {
     cin>>array[elem];
     }
-    average(array,length);
+    choice(array,length);
 }
+
 void even(int array[],int length){
 cout<<endl;
 for(int elem=0;elem<length;elem++){   
@@ -28,6 +38,7 @@ for(int elem=0;elem<length;elem++){
     
 }
 cout<<endl;
+choice(array,length);
 }
 
 void odd(int array[],int length){
@@ -40,7 +51,8 @@ for(int elem=0;elem<length;elem++){
     }
     
 }
-cout<<endl;
+cout<<'\n'<<endl;
+choice(array,length);
 }
 
 void sum(int array[],int length){
@@ -48,7 +60,8 @@ void sum(int array[],int length){
     for(int elem=0;elem<length;elem++){
         sum_terms+=array[elem];
     }
-cout<<"Sum of all terms in array is : "<<sum_terms<<endl; 
+cout<<"Sum of all terms in array is : "<<sum_terms<<'\n'<<endl; 
+choice(array,length);
 }
 
 void average(int array[],int length){
@@ -58,8 +71,31 @@ void average(int array[],int length){
         sum_terms+=array[elem];
     }
     avg=(sum_terms/length);
-    cout<<"Average of all terms is : "<<avg<<endl;
-    
+    cout<<"Average of all terms is : "<<avg<<'\n'<<endl;
+    choice(array,length);
+}
+void choice(int array[],int length){
+    int option;
+    cout<<" 1 : Even Elements \n 2 : Odd Elements \n 3 : Sum of all elements \n 4 : Average of all elements ";
+    cout<<"Enter what operation you want : ";
+    cin>>option;
+    if (option==1)
+    {
+        even(array,length);
+    }
+    else if (option==2)
+    {
+        odd(array,length);
+    }
+    else if (option==3)
+    {
+        sum(array,length);
+    }
+    else if(option==4);
+    {
+        average(array,length);
+    }
+
 }
 
 
